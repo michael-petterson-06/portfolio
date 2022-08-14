8c9d0ea0d4e819a58cac2bc17e0d209348ff7c2c
Michael <michaelpetterson06.com.br>
2022-08-13 21:31:19 -0300

    sidebar

--- react_sass/src/App.jsx
+++ react_sass/src/App.jsx
@@ -1,16 +1,19 @@
-import MainContent from './components/Maincontent';
-import SideBar from './components/Sidebar';
-import './styles/components/app.sass';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import MainContent from "./components/MainContent";
+
+import Sidebar from "./components/Sidebar";
+import "./styles/components/app.sass";
 
 function App() {
-
   return (
+    <Router>
     <div id="portfolio">
-        <h1>Michael Petterson</h1>
-        <SideBar/>
-        <MainContent/>
+      <h1>Michael Petterson</h1>
+      <Sidebar />
+      <MainContent />
     </div>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
