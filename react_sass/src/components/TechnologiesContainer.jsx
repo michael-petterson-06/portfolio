4adb1b687a30a00fd77524293ed590d49bf5c9c6
Michael <michaelpetterson06.com.br>
2022-08-24 12:45:01 -0300

    estilizando TechnologiesContainer

--- react_sass/src/components/TechnologiesContainer.jsx
+++ react_sass/src/components/TechnologiesContainer.jsx
@@ -1,6 +1,5 @@
 import {
     DiHtml5,
-    DiCss3,
     DiJsBadge,
     DiNodejsSmall,
     DiMysql,
@@ -8,18 +7,20 @@ import {
     DiMongodb,
     DiPhp,
   } from "react-icons/di";
+
+  import { SiJest } from "react-icons/si";
   
   import "../styles/components/technologiescontainer.sass";
   
   const technologies = [
-    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-    { id: "css", name: "CSS3", icon: <DiCss3 /> },
-    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-    { id: "react", name: "React", icon: <DiReact /> },
-    { id: "mongodb", name: "Mongodb", icon: <DiMongodb /> },
-    { id: "php", name: "Php", icon: <DiPhp /> },
+    { id: "html", name: "HTML5", icon: <DiHtml5 />, descrition: 'Linguagem de marcação com objetivos de facilitar a manipulação dos elementos.' },
+    { id: "css", name: "JEST", icon: <SiJest />, descrition: 'É um framework de teste em JavaScrip. ' },
+    { id: "js", name: "JavaScript", icon: <DiJsBadge />, descrition: 'Permite a você implementar itens complexos em páginas web' },
+    { id: "node", name: "Node.js", icon: <DiNodejsSmall />, descrition: 'Uma plataforma de javascript construída lado do servidor.' },
+    { id: "mysql", name: "MySQL", icon: <DiMysql />, descrition: 'É um sistema de gerenciamento de banco de dados, que utiliza SQL.' },
+    { id: "react", name: "React", icon: <DiReact />, descrition: 'É uma biblioteca JavaScript com foco em criar páginas web ' },
+    { id: "mongodb", name: "Mongodb", icon: <DiMongodb />, descrition: 'MongoDB é um software de banco de dados nosql orientado a documentos.' },
+    { id: "php", name: "Php", icon: <DiPhp />, descrition: 'PHP é uma linguagem de aplicações atuantes no lado do servidor.' },
   ];
   
   const TechnologiesContainer = () => {
@@ -32,7 +33,7 @@ import {
               {tech.icon}
               <div className="technology-info">
                 <h3>{tech.name}</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                {/* <p>{tech.descrition}</p> */}
               </div>
             </div>
           ))}
